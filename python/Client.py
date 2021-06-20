5913e94426416163180d5cd2b916c20cda0f62f2
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-20 18:16:21 +1000

    '

--- python/Client.py
+++ python/Client.py
@@ -4,7 +4,7 @@ import socket
 TCP_IP = '192.168.1.29' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+MESSAGE = input("Please enter your message: ")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
