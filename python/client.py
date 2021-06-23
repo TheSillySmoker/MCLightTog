0ce0a1fbb11ccb0f1da015b4ccd5cb4b4b9c5744
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-23 20:50:26 +1000

    asadf

--- python/client.py
+++ python/client.py
@@ -1,11 +1,5 @@
 import socket
-import RPi.GPIO as GPIO
-import time
 
-ledPin = 16
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(ledPin, GPIO.OUT)
-GPIO.setwarnings(False)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.29", 1234))
