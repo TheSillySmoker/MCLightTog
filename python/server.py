b1bc0bcc2ef14a20167883fd2563537abac97cce
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-23 19:07:57 +1000

    test

--- python/server.py
+++ python/server.py
@@ -1,22 +1,14 @@
 import socket
 
-TCP_IP = '192.168.1.19' # this IP of my pc. When I want raspberry pi 2`s as a server, I replace it with its IP '169.254.54.195'
-TCP_PORT = 50002
-BUFFER_SIZE = 20 # Normally 1024, but I want fast response
-while 1:
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((TCP_IP, TCP_PORT))
-    s.listen(1)
-
-    conn, addr = s.accept()
-    print ('Connection address:', addr)
-    while 1:
-        data = conn.recv(BUFFER_SIZE)
-        if not data: break
-
-        conn.send(data)  # echo
-        print(data.decode('utf-8'))
-    conn.close()
+while True:
+    # now our endpoint knows about the OTHER endpoint.
+    clientsocket, address = s.accept()
+    print(f"Connection from {address} has been established.")
+    clientsocket.send(bytes("Hello!!!!", "utf-8"))
+    clientsocket.close
 
     
