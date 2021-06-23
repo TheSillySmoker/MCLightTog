dea5b4aa42ab6fea56838266ffbd3d3ea49bf5d1
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-23 20:40:39 +1000

    asdf

--- python/client.py
+++ python/client.py
@@ -1,9 +1,19 @@
 import socket
+import RPi.GPIO as GPIO
+import time
+
+ledPin = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.29", 1234))
 
 while 1:
     msg = s.recv(1024)
+    decMsg = msg.decode("utf-8")
     print(msg.decode("utf-8"))
-    s.send(bytes("Right back at ya bitch!", "utf-8"))
+    if (decMsg == "On"):
+        GPIO.output(ledPin, GPIO.HIGH)
+    if (decMsg == "Off"):
+        GPIO.output(ledPin, GPIO.LOW)
