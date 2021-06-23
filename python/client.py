b103a561252ad38bb609c50e8b7cee6b2126d902
TheSillySmoker <thesillysmoker@gmail.com>
2021-06-23 20:08:48 +1000

    asdf

--- python/client.py
+++ python/client.py
@@ -3,5 +3,6 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("192.168.1.29", 1234))
 
-msg = s.recv(1024)
-print(msg.decode("utf-8"))
+while 1:
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
--- python/server.py
+++ python/server.py
@@ -1,14 +1,11 @@
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1234))
+s.bind(("192.168.1.29", 1234))
 s.listen(5)
 
 while True:
-    # now our endpoint knows about the OTHER endpoint.
     clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Hello!!!!", "utf-8"))
-    clientsocket.close
-
-    
+    print(f"Connection from {address} has been established!")
+    while 1:
+        clientsocket.send(bytes(input("Please enter a message: "), "utf-8"))
